616a8c4a516432e3dab2420dc82ed58b98e93ed9
Deep Raj <deepraj6897@gmail.com>
2025-03-21 09:25:22 +0530

    implemented assets in file

---
+++ src/assets/assest.js
@@ -0,0 +1,43 @@
+import history from './history.png'
+import menu_icon from './menu.png'
+import plus_icon from './plus.png'
+import question from './askquestion.png'
+import message from './chat.png'
+import code from './code.png'
+import send from './send.png'
+import settings from './settings.png'
+import bulb_icon from './lightbulb.png'
+import compass from './compass.png'
+import gallery from './gallery.png'
+import gallery_icon from './galleryicon.png'
+import gemini_icon from './geminiicon.png'
+import compass_icon from './compass.png'
+import menu from './menu.png'
+import mic from './mic.png'
+import youtube_icon from './youtube.png'
+import user1_icon from './user1.png'
+import user2_icon from './user2.png'
+import user3_icon from './user3.png'
+
+export const assets = {
+    history, 
+    menu, 
+    mic,
+    youtube_icon,
+    compass_icon,
+    menu_icon,
+    plus_icon,
+    question,
+    message,
+    code,
+    send,
+    settings,
+    bulb_icon,
+    compass,
+    gallery,
+    gallery_icon,
+    gemini_icon,
+    user1_icon,
+    user2_icon,
+    user3_icon,
+}
