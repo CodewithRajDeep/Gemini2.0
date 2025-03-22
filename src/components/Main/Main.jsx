d0a581c3fd6da67dde54f3d0255254caba51ece5
Deep Raj <deepraj6897@gmail.com>
2025-03-22 09:29:17 +0530

    main component added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,30 @@ const Main = () => {
             <p>Gemini2.0</p>
             <img src={assets.user1_icon} alt="" />
         </div>
-      
+      <div className="main-container">
+        <div className="greet">
+            <p><span>Hello!</span></p>
+            <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Suggest scripting mechanism to be used while working on so</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>How to define a function in a script</p>
+                <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Concepts of System Design in brief with examples in detail.</p>
+                <img src={assets.message} alt="" />
+            </div>
+            <div className="card">
+                <p>How often do you think Web3, Blockchain, and AI will be used in the next 5 years?</p>
+                <img src={assets.code} alt="" />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
